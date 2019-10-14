69bf1e0dc4e6ba6ba2fff575722c86a6809f6338
Larin Dmitry <larin.dmitry.7@ya.ru>
2019-10-14 16:42:44 +0500

    Change FieldDrawerElement(add updateDebugInfo)

--- src/main/kotlin/gui/FieldDrawer.kt
+++ src/main/kotlin/gui/FieldDrawer.kt
@@ -6,4 +6,6 @@ interface FieldDrawer {
     fun generateField(width: Int, height: Int)
     fun drawField(field: List<List<FieldElement>>)
     fun updatedElements(elements: Iterable<FieldCell>)
+    fun updateDebugInfo(string: Iterable<String>)
+    fun changeFoodCount(newFoodCount: Int)
 }
