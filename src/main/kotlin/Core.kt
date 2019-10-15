0deab55805338d04bca978bb6e2450827bf045f1
PesGospoda <kanniball@bk.ru>
2019-10-15 06:35:03 +0500

    realize 0.61

--- src/main/kotlin/Core.kt
+++ src/main/kotlin/Core.kt
@@ -1,2 +1,17 @@
+import gui.GUI
+import logic.Algorithm
+import logic.Game
+import java.util.*
+
 object Core {
+    fun generateGame(w: Int, h: Int){
+        val algorithm = Algorithm.generateRandomAlgorithm()
+        GUI.generateField(w, h)
+        val game = Game(w, h, 30, algorithm, 250, energyForOneFood = 20)
+        Timer().schedule(object : TimerTask() {
+            override fun run(){
+                game.tick()
+            }
+        }, 0, 50)
+    }
 }
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -6,22 +6,5 @@ import gui.GUI
 import java.util.*
 
 fun main() {
-    val algorithm = Algorithm.generateRandomAlgorithm()
-    GUI.generateField(150, 50)
-    val game = Game(150, 50, 30, algorithm, 250, energyForOneFood = 20)
-
-//    val initTime = System.currentTimeMillis()
-//    for (i in 0 until 100) {
-//        game.tick()
-//    }
-
-    Timer().schedule(object : TimerTask() {
-        override fun run(){
-            game.tick()
-        }
-    }, 0, 50)
-
-
-//    val finishTime = System.currentTimeMillis()
-//    println("Millis for 100 ticks: ${finishTime - initTime}")
+    Core.generateGame(50, 50)
 }
